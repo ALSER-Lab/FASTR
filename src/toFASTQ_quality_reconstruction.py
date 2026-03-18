Mode 1 reconstruction working w/ newline changes. 255 representing naturally occurring 10's have also been fixed in scaling logic.

src/toFASTQ_quality_reconstruction.py
@@ -44,14 +44,14 @@ def build_formula_func(formula: str):
 def reverse_scaling_to_quality(binary_values, subtract_table, inverse_table, max_phred):
     """
     Convert binary encoded values back to PHRED quality scores.
-
     WARNING: This function is JIT-compiled with @njit. Do not use Python objects,
     lists, dicts, or advanced numpy operations. Only basic numpy arrays and operations are supported
-
     Returns: np.ndarray of reconstructed PHRED quality scores
     """
-    y = binary_values - subtract_table[binary_values]
-
+    remapped = np.empty_like(binary_values)
+    for i in range(binary_values.shape[0]):
+        remapped[i] = 10 if binary_values[i] == 255 else binary_values[i]
+    y = remapped - subtract_table[remapped]
     # Clip between 0 and 63
     y_clipped = np.empty_like(y)
     for i in range(y.shape[0]):
@@ -61,7 +61,6 @@ def reverse_scaling_to_quality(binary_values, subtract_table, inverse_table, max
         elif val > 63:
             val = 63
         y_clipped[i] = val
-
     # Map through inverse table
     result = np.empty_like(y_clipped)
     for i in range(y_clipped.shape[0]):
@@ -71,7 +70,6 @@ def reverse_scaling_to_quality(binary_values, subtract_table, inverse_table, max
         elif val > max_phred:
             val = max_phred
         result[i] = val
-
     return result
 
 